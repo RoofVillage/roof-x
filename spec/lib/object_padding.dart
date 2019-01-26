7ee5cea540aa0d3964ee247b8890cf34b50a6f41
Joao <ritterjv@gmail.com>
2019-01-26 01:28:34 -0800

    const fix

--- spec/lib/object_padding.dart
+++ spec/lib/object_padding.dart
@@ -2,18 +2,12 @@ import 'package:flutter/material.dart';
 import 'distance.dart';
 
 class RoofObjectPadding {
-  static EdgeInsets get field1 {
-    return EdgeInsets.fromLTRB(
-        RoofDistance.b, RoofDistance.d, RoofDistance.b, RoofDistance.d);
-  }
+  static const EdgeInsets field1 = EdgeInsets.fromLTRB(
+      RoofDistance.b, RoofDistance.d, RoofDistance.b, RoofDistance.d);
 
-  static EdgeInsets get field2 {
-    return EdgeInsets.fromLTRB(
-        RoofDistance.b, RoofDistance.a, RoofDistance.b, RoofDistance.c);
-  }
+  static const EdgeInsets field2 = EdgeInsets.fromLTRB(
+      RoofDistance.b, RoofDistance.a, RoofDistance.b, RoofDistance.c);
 
-  static EdgeInsets get container1 {
-    return EdgeInsets.fromLTRB(
-        RoofDistance.c, RoofDistance.c, RoofDistance.c, RoofDistance.c);
-  }
+  static const EdgeInsets container1 = EdgeInsets.fromLTRB(
+      RoofDistance.c, RoofDistance.c, RoofDistance.c, RoofDistance.c);
 }
