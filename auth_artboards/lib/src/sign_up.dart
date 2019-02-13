9743d7f6086df0a9fb070aacde429e33c0695e48
Tyler Hayes <tylerxhayes@gmail.com>
2019-02-12 20:03:38 -0800

    form field listeners for passing field values back to fieldData are working

--- auth_artboards/lib/src/sign_up.dart
+++ auth_artboards/lib/src/sign_up.dart
@@ -16,17 +16,25 @@ class SignUpArtboard extends FormFloatingArtboard {
     final _nameFieldData = FormTextFieldData(
         size: 1 / 2,
         title: "Full name",
-        onChanged: (someString) => print(someString));
+        onChanged: (someString) => print("Textfield: $someString"));
 
     final _goodFieldData = FormTextFieldData(
         size: 1 / 2,
         title: "Good name",
-        onChanged: (someString) => print(someString));
+        onChanged: (someString) => print("Textfield2: $someString"));
 
-    final _textArea = FormTextAreaData(title: "Text area");
+    final _textArea = FormTextAreaData(
+        title: "Text area",
+        onChanged: (someString) => print("textarea: $someString"));
 
-    final _switch1 = FormSwitchData(title: "Turn dis on", initialValue: false);
-    final _switch2 = FormSwitchData(title: "Turn dis off", initialValue: true);
+    final _switch1 = FormSwitchData(
+        title: "Turn dis on",
+        initialValue: false,
+        onChanged: (isOn) => print("switch $isOn"));
+    final _switch2 = FormSwitchData(
+        title: "Turn dis off",
+        initialValue: true,
+        onChanged: (isOn) => print("switch $isOn"));
     final _selectField = FormOptionSelectData(title: "Pick yo shit", options: [
       FormOptionSelectValueData(title: "One"),
       FormOptionSelectValueData(title: "two")
