c263add5bb0ee6864bf3317503bdf834ddbd494a
Joao <ritterjv@gmail.com>
2019-03-08 20:52:31 -0800

    added currency folder

---
+++ currency/lib/src/currency.dart
@@ -0,0 +1,9 @@
+// import 'package:money/money.dart';
+// import 'package:intl/intl.dart';
+// import 'package:locales/currency_codes.dart';
+
+// class K {
+//   final Money money;
+
+//   K({Int amount}) : money = Money(amount, NumberFormat.currency(locale:);
+// }
