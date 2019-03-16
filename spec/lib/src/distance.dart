2b0c108e4834de787d5aa664abeb9338d53558b2
Tyler Hayes <tylerxhayes@gmail.com>
2019-03-15 21:25:28 -0700

    update sdk constraints and add decimal places to type double initializations to fix flutter bug reporting

--- spec/lib/src/distance.dart
+++ spec/lib/src/distance.dart
@@ -1,9 +1,9 @@
 class RoofDistance {
-  static const double a = 4;
-  static const double b = 8;
-  static const double c = 12;
-  static const double d = 20;
-  static const double e = 40;
-  static const double f = 70;
-  static const double g = 100;
+  static const double a = 4.0;
+  static const double b = 8.0;
+  static const double c = 12.0;
+  static const double d = 20.0;
+  static const double e = 40.0;
+  static const double f = 70.0;
+  static const double g = 100.0;
 }
