acab449973371b909c89f69689e82a932e030718
Tyler Hayes <tylerxhayes@gmail.com>
2019-04-21 12:14:53 -0700

    rename thread cells

--- builders/table/lib/index.dart
+++ builders/table/lib/index.dart
@@ -4,8 +4,8 @@ export 'src/data/index.dart';
 
 // TODO export for testing only
 export 'src/_components/_cells/activity.dart';
-export 'src/_components/_cells/threadComment.dart';
-export 'src/_components/_cells/threadEvent.dart';
-export 'src/_components/_cells/standardThread.dart';
-export 'src/_components/_cells/dueSoonThread.dart';
-export 'src/_components/_cells/overdueThread.dart';
+export 'src/_components/_cells/thread_comment.dart';
+export 'src/_components/_cells/thread_event.dart';
+export 'src/_components/_cells/standard_thread.dart';
+export 'src/_components/_cells/due_soon_thread.dart';
+export 'src/_components/_cells/overdue_thread.dart';
