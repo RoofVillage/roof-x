0a1ea48e0802ccc34e5d86fee0bf2a915f40d2cd
Tyler Hayes <tylerxhayes@gmail.com>
2019-05-25 11:52:41 -0700

    breadcrumb tap handling update

--- builders/breadcrumb_stack/lib/src/_components/breadcrumb_stack.dart
+++ builders/breadcrumb_stack/lib/src/_components/breadcrumb_stack.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:theme/index.dart';
 import 'package:icon_library/index.dart';
+import 'package:haptics/index.dart';
 import 'package:distance/index.dart' as distance;
 
 import '../breadcrumb.dart';
 
 class RoofBreadcrumbStack extends StatelessWidget {
   final List<RoofBreadcrumb> breadcrumbs;
-  final String title;
+  final VoidCallback onTap;
 
   RoofBreadcrumbStack({
     this.breadcrumbs,
-    this.title,
+    this.onTap,
   });
 
   final _verticalMargin = distance.c;
   final _verticalSpacing = distance.b;
+  final _tapHapticOption = HapticOption.light;
+
+  void _fireHaptic() {
+    if (onTap != null) triggerHapticWith(_tapHapticOption);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,13 +50,17 @@ class RoofBreadcrumbStack extends StatelessWidget {
       );
     }
 
-    return Container(
-      color: theme.color.background.generalPrimary,
-      padding: EdgeInsets.symmetric(vertical: _verticalMargin),
-      child: Wrap(
-        runSpacing: _verticalSpacing,
-        children: wrapChildren,
-        crossAxisAlignment: WrapCrossAlignment.center,
+    return GestureDetector(
+      onTapDown: (details) => _fireHaptic(),
+      onTap: onTap,
+      child: Container(
+        color: theme.color.background.generalPrimary,
+        padding: EdgeInsets.symmetric(vertical: _verticalMargin),
+        child: Wrap(
+          runSpacing: _verticalSpacing,
+          children: wrapChildren,
+          crossAxisAlignment: WrapCrossAlignment.center,
+        ),
       ),
     );
   }
--- builders/breadcrumb_stack/lib/src/breadcrumb.dart
+++ builders/breadcrumb_stack/lib/src/breadcrumb.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:icon_library/index.dart';
 import 'package:theme/index.dart';
-import 'package:haptics/index.dart';
 import 'package:typography/index.dart' as typography;
 import 'package:distance/index.dart' as distance;
 
 class RoofBreadcrumb extends StatelessWidget {
   final String title;
   final XSmallIconReference iconReference;
-  final VoidCallback onTap;
 
   RoofBreadcrumb({
     this.title,
     this.iconReference,
-    this.onTap,
   });
 
   final _typographyStyle = typography.bodySecondary;
   final _horizontalSpacing = distance.b;
   final _horizontalMargin = distance.b;
-  final _tapHapticOption = HapticOption.light;
-
-  void _fireHaptic() {
-    if (onTap != null) triggerHapticWith(_tapHapticOption);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,15 +44,11 @@ class RoofBreadcrumb extends StatelessWidget {
 
     rowChildren.add(textWidget);
 
-    return GestureDetector(
-      onTapDown: (details) => _fireHaptic(),
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.only(left: _horizontalMargin),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: rowChildren,
-        ),
+    return Container(
+      padding: EdgeInsets.only(left: _horizontalMargin),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: rowChildren,
       ),
     );
   }
