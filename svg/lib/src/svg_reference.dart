49c09b9645995fbe9e8bce25c64733d02fd58f7e
Tyler Hayes <tylerxhayes@gmail.com>
2019-06-14 21:44:54 -0700

    rework dependencies and asset management for new icon and svg libraries

--- svg/lib/src/svg_reference.dart
+++ svg/lib/src/svg_reference.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SvgReference extends _SvgReference {
-  static const _localPath = "lib/src/";
-
+  static const _localPath = "lib/src/_assets/";
   static const double _size = 32;
 
   SvgReference({String path = "", double width = _size, double height = _size})
-      : super(path: _localPath + "custom/$path", height: height, width: width);
+      : super(path: _localPath + path, height: height, width: width);
 
-  static final logoFull = SvgReference(path: 'logo_full.svg', width: 65);
+  static final logoFull = SvgReference(path: "logo_full.svg", width: 65);
 }
 
 class _SvgReference {
