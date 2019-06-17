3b3730e15cf495cfdd68365e7bb1e62ac2cfa1dd
Tyler Hayes <tylerxhayes@gmail.com>
2019-06-16 18:22:18 -0700

    add shopping item artboards. organize artboards in roof, organization, and shared folders

--- artboards/lease/lib/src/_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:schedule/index.dart';
-
-mixin LeaseArtboardData {
-  String get leaseTitle;
-  String get homeTitle;
-  int get totalAmount;
-  int get startTimestamp;
-  int get endTimestamp;
-}
