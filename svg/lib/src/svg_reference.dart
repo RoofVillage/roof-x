1db5d8359de1e8bd327e52f22e8bf50fc5bb18b9
Joao <ritterjv@gmail.com>
2019-11-05 20:33:34 -0800

    -

--- svg/lib/src/svg_reference.dart
+++ svg/lib/src/svg_reference.dart
@@ -22,7 +22,7 @@ class _SvgReference {
     if (path != null && path.endsWith(".svg")) {
       return SvgPicture.asset(
         path,
-        package: "svg",
+        package: "svg_library",
         height: height,
         width: width,
         color: color,
