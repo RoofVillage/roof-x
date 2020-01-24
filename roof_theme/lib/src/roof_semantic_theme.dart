f204ca9df65decde75b10fb775e5ff5ce224b829
Tyler Hayes <tylerxhayes@gmail.com>
2020-01-23 19:26:36 -0800

    add roof shadow library

--- roof_theme/lib/src/roof_semantic_theme.dart
+++ roof_theme/lib/src/roof_semantic_theme.dart
@@ -4,7 +4,9 @@ import 'package:theme/src/color/roof_color_library.dart';
 import 'package:theme/src/curve/roof_curve_library.dart';
 import 'package:theme/src/distance/roof_distance_library.dart';
 import 'package:theme/src/duration/roof_duration_library.dart';
+import 'package:theme/src/radius/roof_duration_library.dart';
 import 'package:theme/src/roof_theme_option.dart';
+import 'package:theme/src/shadow/roof_shadow_library.dart';
 
 class RoofSemanticTheme extends SemanticTheme<RoofThemeOption> {
   RoofSemanticTheme(
@@ -34,10 +36,10 @@ class RoofInheritedSemanticTheme
   SemanticDurationLibrary get durations => RoofDurationLibrary();
 
   @override
-  SemanticRadiusLibrary get radiuses => null;
+  SemanticRadiusLibrary get radiuses => RoofRadiusLibrary();
 
   @override
-  SemanticShadowLibrary get shadows => null;
+  SemanticShadowLibrary get shadows => RoofShadowLibrary(currentThemeOption);
 
   @override
   SemanticTypographyLibrary get typography => null;
