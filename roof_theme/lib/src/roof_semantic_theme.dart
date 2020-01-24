71684d5f210a65794b3dd0f54573b155e922ffad
Tyler Hayes <tylerxhayes@gmail.com>
2020-01-23 19:17:08 -0800

    add roof duration library

--- roof_theme/lib/src/roof_semantic_theme.dart
+++ roof_theme/lib/src/roof_semantic_theme.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:semantic_theme/index.dart';
 import 'package:theme/src/color/roof_color_library.dart';
 import 'package:theme/src/curve/roof_curve_library.dart';
+<<<<<<< HEAD
 import 'package:theme/src/distance/roof_distance_library.dart';
 import 'package:theme/src/duration/roof_duration_library.dart';
 import 'package:theme/src/radius/roof_duration_library.dart';
+||||||| merged common ancestors
+=======
+import 'package:theme/src/distance/roof_distance_library.dart';
+import 'package:theme/src/duration/roof_duration_library.dart';
+>>>>>>> add roof duration library
 import 'package:theme/src/roof_theme_option.dart';
 import 'package:theme/src/shadow/roof_shadow_library.dart';
 import 'package:theme/src/system_ui_overlay_style/roof_system_ui_overlay_style.dart';
