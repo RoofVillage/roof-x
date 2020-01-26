c16043711cbc5fc8d0df441c58117ac75afa58bd
Tyler Hayes <tylerxhayes@gmail.com>
2020-01-25 23:18:04 -0800

    update public activity artboard to use button builder
    pass args in button stack builder

--- artboards/public_activity/lib/src/full_screen.dart
+++ artboards/public_activity/lib/src/full_screen.dart
@@ -1,4 +1,4 @@
-import 'package:button/index.dart';
+import 'package:button_builder/index.dart';
 import 'package:empty_table_vertical_full_screen_artboard_template/index.dart';
 import 'package:flutter/material.dart';
 import 'package:button_stack_builder/index.dart';
@@ -10,67 +10,45 @@ import '_data.dart';
 
 class PublicActivityVerticalFullScreenArtboard
     extends EmptyTableVerticalFullScreenArtboard
-    with PublicActivityArtboardData, ButtonStackBuilder {
-  // List<Widget> buildActionButtons(BuildContext context) {
-  //   final button1 = buildIconNavButton(
-  //     context,
-  //     iconReference: NavigationIcon.add,
-  //     onTap: (context) {
-  //       ArtboardNavigator.of(context).goTo(
-  //         SignUpVerticalFloatingArtboard(),
-  //       );
-  //     },
-  //   );
-
-  //   final button2 = buildIconNavButton(
-  //     context,
-  //     iconReference: NavigationIcon.settings,
-  //     onTap: (context) {
-  //       ArtboardNavigator.of(context).goTo(
-  //         PublicActivityVerticalFullScreenArtboard(),
-  //       );
-  //     },
-  //   );
-
-  //   final popButton = buildIconNavButton(
-  //     context,
-  //     iconReference: NavigationIcon.backArrow,
-  //     onTap: (context) {
-  //       ArtboardNavigator.of(context).pop(context);
-  //     },
-  //   );
-
-  //   return [button1, button2, popButton];
-  // }
-
-  @override
-  List<Widget> get buttonStackButtons => [
-        RoofPrimaryCenterButton(
-          onTap: (context) {
-            ArtboardNavigator.of(context).goTo(
-              SignUpVerticalFloatingArtboard(),
-            );
-          },
-          icon: XSmallIcon.house,
-          text: "I'm a roommate",
-        ),
-        RoofPrimaryCenterButton(
-          onTap: (context) => print('login'),
-          icon: XSmallIcon.house,
-          text: "I'm a tenant",
-        ),
-        RoofPrimaryCenterButton(
-          onTap: (context) => print('login'),
-          icon: XSmallIcon.house,
-          text: "I'm a landlord",
-        ),
-        RoofSecondaryCenterButton(
-          onTap: (context) => print('login'),
-          icon: XSmallIcon.house,
-          text: "Log in",
-        ),
-      ];
+    with
+        PublicActivityArtboardData,
+        ButtonStackBuilder,
+        PrimaryCenterButtonBuilder {
+  List<Widget> Function(BuildContext) get buildButtonStackButtons =>
+      (BuildContext context) => [
+            buildPrimaryCenterButton(
+              context,
+              onTap: (context) {
+                ArtboardNavigator.of(context).goTo(
+                  SignUpVerticalFloatingArtboard(),
+                );
+              },
+              icon: XSmallIcon.house,
+              text: "I'm a roommate",
+            ),
+            buildPrimaryCenterButton(
+              context,
+              onTap: (context) => print('login'),
+              icon: XSmallIcon.house,
+              text: "I'm a tenant",
+            ),
+            buildPrimaryCenterButton(
+              context,
+              onTap: (context) => print('login'),
+              icon: XSmallIcon.house,
+              text: "I'm a landlord",
+            ),
+            buildPrimaryCenterButton(
+              context,
+              onTap: (context) => print('login'),
+              icon: XSmallIcon.house,
+              text: "Log in",
+            ),
+          ];
 
   @override
-  Widget buildDock(BuildContext context) => buildButtonStack(context);
+  Widget buildDock(BuildContext context) => buildButtonStack(
+        context,
+        buildButtonStackButtons(context),
+      );
 }
--- builders/button_stack/lib/src/button_stack_builder.dart
+++ builders/button_stack/lib/src/button_stack_builder.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 import '_components/button_stack.dart';
 
 mixin ButtonStackBuilder {
-  List<Widget> get buttonStackButtons;
-
-  RoofButtonStack buildButtonStack(BuildContext context) {
-    return RoofButtonStack(buttons: buttonStackButtons);
+  RoofButtonStack buildButtonStack(
+    BuildContext context,
+    List<Widget> buttons,
+  ) {
+    return RoofButtonStack(
+      buttons: buttons,
+    );
   }
 }
